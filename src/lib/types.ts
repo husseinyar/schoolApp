f87a20e7b0d40a1fc7a9b860c73a322cdef4ee4e
husseinyar <hussein.abdi.gele@gmail.com>
2026-04-10 05:39:17 +0200

    fix it erro role types 2error

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,6 +1,11 @@
-export enum Role {
-  ADMIN = "ADMIN",
-  DRIVER = "DRIVER",
-  PARENT = "PARENT",
-  STUDENT = "STUDENT",
-}
+import { Role as PrismaRole } from "@prisma/client";
+
+// This makes the two types identical in the eyes of TypeScript
+export type Role = PrismaRole; 
+export const Role = {
+  ADMIN: 'ADMIN',
+  TEACHER: 'TEACHER',
+  PARENT: 'PARENT',
+  STUDENT: 'STUDENT',
+  DRIVER: "DRIVER", 
+} as const;
